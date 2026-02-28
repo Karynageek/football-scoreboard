838a72b10d4f03746ef142b039b974caf97f4fb8
Kary User <karyna.chelpan@esto.eu>
2026-02-28 14:37:44 +0200

    added updates for startGame and finishGame

--- src/Application/ScoreBoardService.php
+++ src/Application/ScoreBoardService.php
@@ -10,9 +10,20 @@ class ScoreBoardService
 
     public function startGame(string $homeTeam, $awayTeam): void
     {
-        if (!isset($this->games[$homeTeam . '-' . $awayTeam])) {
-            $this->games[$homeTeam . '-' . $awayTeam] = new Game(0, 0);
+        if (isset($this->games[$homeTeam . '-' . $awayTeam])) {
+            throw new \Exception('Game already started');
         }
+
+        $this->games[$homeTeam . '-' . $awayTeam] = new Game(0, 0);
+    }
+
+    public function finishGame(string $homeTeam, $awayTeam): void
+    {
+        if (!isset($this->games[$homeTeam . '-' . $awayTeam])) {
+            throw new \Exception('Game not started');
+        }
+
+        unset($this->games[$homeTeam . '-' . $awayTeam]);
     }
 
     public function getGames(): array
--- tests/Unit/Application/ScoreBoardServiceTest.php
+++ tests/Unit/Application/ScoreBoardServiceTest.php
@@ -18,4 +18,45 @@ class ScoreBoardServiceTest extends TestCase
         /* ASSERT */
         $this->assertCount(1, $scoreBoardService->getGames());
     }
+
+    #[Test]
+    public function it_throws_an_exception_if_game_already_started()
+    {
+        /* SETUP */
+        $scoreBoardService = new ScoreBoardService();
+        $scoreBoardService->startGame('Mexico', 'Canada');
+
+        /* ASSERT */
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Game already started');
+
+        /* EXECUTE */
+        $scoreBoardService->startGame('Mexico', 'Canada');
+    }
+
+    #[Test]
+    public function it_removes_from_score_board_when_game_finished()
+    {
+        /* SETUP */
+        $scoreBoardService = new ScoreBoardService();
+        $scoreBoardService->startGame('Mexico', 'Canada');
+
+        /* EXECUTE */
+        $scoreBoardService->finishGame('Mexico', 'Canada');
+
+        /* ASSERT */
+        $this->assertCount(0, $scoreBoardService->getGames());
+    }
+
+    #[Test]
+    public function it_throws_an_exception_if_game_not_started_and_finished()
+    {
+        /* ASSERT */
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Game not started');
+
+        /* EXECUTE */
+        $scoreBoardService = new ScoreBoardService();
+        $scoreBoardService->finishGame('Mexico', 'Canada');
+    }
 }
